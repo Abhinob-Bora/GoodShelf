958e4f7547d51a0ccd69ae27a8dbacb1adccb3cd
Abhinob-Bora <abhinobbora6@gmail.com>
2023-08-27 14:14:39 +0530

    git me add kar diya

---
+++ Backend/models/productModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter a product name"]
+        },
+        OneLiner: {
+            type: String,
+            required: true,
+        },
+
+        quantity: {
+            type: String,
+            required: true,
+            default: 0
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+        Section: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
